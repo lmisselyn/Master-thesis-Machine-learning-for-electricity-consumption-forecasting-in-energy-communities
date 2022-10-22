a829eb8e1357c1938e8ffb63608c9b80664bcbea
lambyCara <lambert.misselyn@hotmail.com>
2022-10-22 12:38:06 +0200

    start

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def data_preprocessing():
+    df = pandas.read_csv("../donneeconso10.csv")
+    print(df["Date"])
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data_preprocessing()
