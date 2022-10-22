faff5baf7560f073974eae43c8b259c26b3a9f88
lmisselyn <lmisselyn@users.noreply.github.com>
2022-10-22 13:59:14 +0200

    select data

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import pandas
+import pandas as pd
 
 def data_preprocessing():
-    df = pandas.read_csv("../donneeconso10.csv")
-    print(df["Date"])
+    df = pd.read_csv("../donneeconso10.csv")[["Date", "Heure", "Index(Wh)"]]
+    one_year = df.loc[34:35296]
+    one_year_df = pd.DataFrame(one_year, columns=df.columns)
+    one_year_df.reset_index(drop=True, inplace=True)
+    one_year_df.to_csv("one_year10.csv")
+
 
 if __name__ == '__main__':
     data_preprocessing()
