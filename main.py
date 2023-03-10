048cf324c6d421542deaf30f46d2b735f3803056
lmisselyn <45001744+lmisselyn@users.noreply.github.com>
2023-03-10 13:17:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     variables = ["Minutes", "Day", "Week", "Weekend", "Month", "Temperature", "Humidity", "Pressure",
                  "Wind speed", "Wind direction", "Snowfall", "Snow depth", "Irradiation", "Rainfall"]
-    selected, acc = helper.select_best_features('one_year_10.csv', random_forest_model, variables=variables)
+    selected, acc = helper.select_best_features('test.csv', random_forest_model, variables=variables)
  
     with open('Select_best_var_RF_10.txt', 'w') as f:
         for i in range(len(selected)):
