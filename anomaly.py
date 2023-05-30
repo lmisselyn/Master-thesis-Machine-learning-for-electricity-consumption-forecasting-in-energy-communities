211620e3450c0424a2ff849353b550f244e9f2a0
lmisselyn <45001744+lmisselyn@users.noreply.github.com>
2023-05-30 14:42:10 +0200

    Update anomaly.py

--- anomaly.py
+++ anomaly.py
@@ -88,8 +88,8 @@ def anomaly_simulator(df, train_n_weeks, n_week, n_days, dataset):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('Datasets/10/10.csv', index_col='Datetime')
-    anomaly_simulator(df['2020-06-09 00:00:00':], 16, 4, 1, '10')
+    df = pd.read_csv('Datasets/09/09.csv', index_col='Datetime')
+    anomaly_simulator(df['2020-06-09 00:00:00':], 16, 4, 1, '09')
 
     #df = pd.read_csv('Datasets/09.csv', index_col='Datetime')
     #anomaly_simulator(df['2020-06-09 00:00:00':], 8, 3, '09_test')
